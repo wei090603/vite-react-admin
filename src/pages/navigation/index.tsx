4b0687744682cfb0c4e7c4d8f059ba9548262eec
wei090603 <1176022890@qq.com>
2022-07-15 23:11:45 +0800

    navgation

--- src/pages/navigation/index.tsx
+++ src/pages/navigation/index.tsx
@@ -1,61 +1,63 @@
 import { FC, useEffect, useState } from "react";
-import { Space, Table, Tag, Button } from "antd";
+import { Space, Table, Button } from "antd";
 import type { ColumnsType } from "antd/lib/table";
 import { getNavgationList } from "@/api/navgation";
 import { INavgation } from "@/api/interface";
 import "./index.less";
 
-const columns: ColumnsType<INavgation.ResNavgationList> = [
-  {
-    title: "导航名",
-    dataIndex: "title",
-    key: "title",
-    render: title => <span>{title}</span>
-  },
-  {
-    title: "链接",
-    dataIndex: "link",
-    key: "link"
-  },
-  {
-    title: "排序",
-    dataIndex: "sort",
-    key: "sort"
-  },
-  {
-    title: "创建日期",
-    dataIndex: "createdAt",
-    key: "createdAt"
-  },
-  {
-    title: "更新日期",
-    dataIndex: "updatedAt",
-    key: "updatedAt"
-  },
-  {
-    title: "操作",
-    key: "action",
-    render: (_, record) => (
-      <Space size="middle">
-        <Button type="primary">编辑</Button>
-        <Button type="primary" danger>
-          删除
-        </Button>
-      </Space>
-    )
-  }
-];
-
 const Navigation: FC = () => {
   const [navgationList, setNavgationList] = useState<INavgation.ResNavgationList[]>([]);
-
-  const [params, setParams] = useState({
-    page: 1
-  });
 
   useEffect(() => {
     getNavgation();
   }, []);
+
+  const columns: ColumnsType<INavgation.ResNavgationList> = [
+    {
+      title: "导航名",
+      dataIndex: "title",
+      key: "title",
+      render: title => <span>{title}</span>
+    },
+    {
+      title: "链接",
+      dataIndex: "link",
+      key: "link"
+    },
+    {
+      title: "排序",
+      dataIndex: "sort",
+      key: "sort"
+    },
+    {
+      title: "创建日期",
+      dataIndex: "createdAt",
+      key: "createdAt"
+    },
+    {
+      title: "更新日期",
+      dataIndex: "updatedAt",
+      key: "updatedAt"
+    },
+    {
+      title: "操作",
+      key: "action",
+      render: (_, record) => (
+        <Space size="middle">
+          <Button type="primary" onClick={() => handleEdit(record)}>
+            编辑
+          </Button>
+          <Button type="primary" danger>
+            删除
+          </Button>
+        </Space>
+      )
+    }
+  ];
+
+  const handleEdit = (row: INavgation.ResNavgationList) => {
+    console.log(row, "row");
+  };
 
   const getNavgation = async () => {
     const data = await getNavgationList();
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,6 @@
 import { useRoutes } from "react-router-dom";
 import { RouteObject } from "./interface";
-// import constantRoutes from "./constantRoutes/platform";
+import constantRoutes from "./constantRoutes/platform";
 
 // * 导入所有router
 const metaRouters = import.meta.globEager("./asyncRoutes/*.tsx");
@@ -13,7 +13,7 @@ Object.keys(metaRouters).forEach(item => {
   });
 });
 
-export const rootRouter: RouteObject[] = [...routerArray];
+export const rootRouter: RouteObject[] = [...routerArray, ...constantRoutes];
 
 const Router = () => {
   const routes = useRoutes(rootRouter);
@@ -21,42 +21,3 @@ const Router = () => {
 };
 
 export default Router;
-
-// import { Navigate, useRoutes } from "react-router-dom";
-// import { RouteObject } from "./interface";
-// import Login from "@/pages/login";
-
-// // * 导入所有router
-// const metaRouters = import.meta.globEager("./modules/*.tsx");
-
-// // * 处理路由
-// export const routerArray: RouteObject[] = [];
-// Object.keys(metaRouters).forEach(item => {
-//   Object.keys(metaRouters[item]).forEach((key: any) => {
-//     routerArray.push(...metaRouters[item][key]);
-//   });
-// });
-
-// export const rootRouter: RouteObject[] = [
-//   {
-//     path: "/login",
-//     element: <Login />,
-//     meta: {
-//       requiresAuth: false,
-//       title: "登录页",
-//       key: "login"
-//     }
-//   },
-//   ...routerArray,
-//   {
-//     path: "*",
-//     element: <Navigate to="/404" />
-//   }
-// ];
-
-// const Router = () => {
-//   const routes = useRoutes(rootRouter);
-//   return routes;
-// };
-
-// export default Router;
